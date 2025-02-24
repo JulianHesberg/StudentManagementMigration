d6eb85e1742e201bbacb978e6e802bf7387aa7e1
LeeVoi <Abual3a7ed.6@gmail.com>
2025-02-24 11:11:19 +0100

    Init the DBContext file for Mapping entities and Schema managment

---
+++ StudentManagementMigration/Data/StudentManagementContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManagementMigration.Models;
+
+namespace StudentManagementMigration.Data
+{
+
+    public class StudentManagementContext : DbContext
+    {
+        public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets represent tables in the database
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Call base method
+            base.OnModelCreating(modelBuilder);
+
+            // Optional: Configure entity relationships, constraints, or default values here.
+            // For example, if you want to make sure that Email is required:
+            // modelBuilder.Entity<Student>().Property(s => s.Email).IsRequired();
+        }
+    }
+}
