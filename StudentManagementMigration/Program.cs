f0745dcf73280e93ae93a9bfb27fea253e2d3e2b
LeeVoi <Abual3a7ed.6@gmail.com>
2025-02-24 11:15:33 +0100

    Init DbConext Config

--- StudentManagementMigration/Program.cs
+++ StudentManagementMigration/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementMigration.Data;
+using Microsoft.Extensions.Configuration;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        // Configure the DbContext with a connection string from configuration
+        services.AddDbContext<StudentManagementContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
+
+        // Add other services as needed.
+    })
+    .Build();
+
+// Now you can retrieve the context from the host's service provider
+using (var scope = host.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<StudentManagementContext>();
+    // Use the dbContext here (e.g., run migrations or seed data)
+}
+
+await host.RunAsync();
