a3e016a448247a41b137c592cd5d9811fb484e43
LeeVoi <Abual3a7ed.6@gmail.com>
2025-03-03 11:33:16 +0100

    Added new Department Relation -> HeadOfDepartmentId

--- StudentManagementMigration/Migrations/20250303102737_AddDepartmentRelation.Designer.cs
+++ StudentManagementMigration/Migrations/20250303102737_AddDepartmentRelation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentManagementMigration.Data;
 
@@ -11,9 +12,11 @@ using StudentManagementMigration.Data;
 namespace StudentManagementMigration.Migrations
 {
     [DbContext(typeof(StudentManagementContext))]
-    partial class StudentManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20250303102737_AddDepartmentRelation")]
+    partial class AddDepartmentRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StudentManagementMigration/Migrations/V6__AddDepartmentRelation.sql
@@ -0,0 +1,19 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [Departments] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Budget] decimal(18,2) NOT NULL,
+    [StartDate] datetime2 NOT NULL,
+    [DepartmentHeadId] int NOT NULL,
+    CONSTRAINT [PK_Departments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Departments_Instructor_DepartmentHeadId] FOREIGN KEY ([DepartmentHeadId]) REFERENCES [Instructor] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Departments_DepartmentHeadId] ON [Departments] ([DepartmentHeadId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250303102737_AddDepartmentRelation', N'9.0.2');
+
+COMMIT;
+GO
+
