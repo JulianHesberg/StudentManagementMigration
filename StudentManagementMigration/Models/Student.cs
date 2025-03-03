ec550020eca101a8ccba832293187719ee4dc676
Julian Hesberg <112935968+JulianHesberg@users.noreply.github.com>
2025-03-03 09:48:30 +0100

    added dateofbirth to student table

--- StudentManagementMigration/Models/Student.cs
+++ StudentManagementMigration/Models/Student.cs
@@ -3,10 +3,8 @@ namespace StudentManagementMigration.Models
 
     public class Student
     {
-        // Primary Key
         public int Id { get; set; }
 
-        // Student Information
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
@@ -14,7 +12,6 @@ namespace StudentManagementMigration.Models
         public DateTime EnrollmentDate { get; set; }
         public DateTime DateOfBirth { get; set; }
 
-        // Navigation Property: A student can have multiple enrollments
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 
