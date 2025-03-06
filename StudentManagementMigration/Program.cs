23da00d00f386cbb15f42896868c70495d3f2217
Abdulkader Alomar <112936101+LeeVoi@users.noreply.github.com>
2025-03-07 00:15:34 +0100

    Update Program.cs

--- StudentManagementMigration/Program.cs
+++ StudentManagementMigration/Program.cs
@@ -7,19 +7,15 @@ using Microsoft.Extensions.Configuration;
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
-        // Configure the DbContext with a connection string from configuration
         services.AddDbContext<StudentManagementContext>(options =>
             options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
 
-        // Add other services as needed.
     })
     .Build();
 
-// Now you can retrieve the context from the host's service provider
 using (var scope = host.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<StudentManagementContext>();
-    // Use the dbContext here (e.g., run migrations or seed data)
 }
 
 await host.RunAsync();
